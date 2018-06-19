153de4915043424e87418b01f8527aeafce355fd
sakshi013 <sakshibhat013@gmail.com>
2018-06-20 00:02:36 +0530

    second commit

--- app/src/main/java/com/example/sakshi/assignment4/MainActivity.java
+++ app/src/main/java/com/example/sakshi/assignment4/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.sakshi.assignment4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Spinner spinnerDropdown;
+        String[] countries ={"USA","China","Japan"};
+        spinnerDropdown =(Spinner)findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,countries);
+
+            spinnerDropdown.setAdapter(adapter);
+
+            spinnerDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int position, long i) {
+                    int id =spinnerDropdown.getSelectedItemPosition();
+                    Toast.makeText(getBaseContext(),"selected countries:" +position,Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+
     }
+
+
 }
